227167a98dbd906572da11cd2ab8bb4150d5e565
viri <hi@viri.moe>
2021-12-27 20:09:22 -0600

    window: add `Window` and builder skeleton

--- src/window.rs
+++ src/window.rs
@@ -1,1 +1,42 @@
-// yup
+/// Represents an open window. Dropping it closes the window.
+///
+/// To instantiate windows, use a [`builder`](Self::builder).
+pub struct Window {
+
+}
+
+impl Window {
+    pub const fn builder() -> WindowBuilder {
+        WindowBuilder::new()
+    }
+}
+
+pub struct WindowBuilder {
+    visible: bool,
+}
+
+impl WindowBuilder {
+    const fn new() -> Self {
+        Self {
+            visible: true,
+        }
+    }
+
+    pub fn build(&self) -> Result<Window, ()> {
+        todo!()
+    }
+
+    /// Sets whether the window is initially visible.
+    ///
+    /// Defaults to `true`.
+    pub fn visible(&mut self, visible: bool) -> &mut Self {
+        self.visible = visible;
+        self
+    }
+}
+
+impl Default for WindowBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
