7f8dbfeba2e48f6367a7e719cd8ed65c773a2e53
viri <hi@viri.moe>
2022-01-07 16:27:44 -0600

    sync: correct scoping with `pub(crate)`

--- src/sync.rs
+++ src/sync.rs
@@ -1,6 +1,6 @@
 //! Wrapper that uses either `parking_lot` or `std` for synchronisation primitives.
 
-pub use self::imp::{cvar_notify_one, cvar_wait, mutex_lock, Condvar, Mutex, MutexGuard};
+pub(crate) use self::imp::{cvar_notify_one, cvar_wait, mutex_lock, Condvar, Mutex, MutexGuard};
 
 #[cfg(not(feature = "parking-lot"))]
 pub(crate) mod imp {
@@ -8,11 +8,11 @@ pub(crate) mod imp {
     pub use std::sync::{Condvar, Mutex, MutexGuard};
 
     #[inline]
-    pub fn cvar_notify_one(cvar: &Condvar) {
+    pub(crate) fn cvar_notify_one(cvar: &Condvar) {
         cvar.notify_one();
     }
 
-    pub fn cvar_wait<T>(cvar: &Condvar, guard: &mut MutexGuard<T>) {
+    pub(crate) fn cvar_wait<T>(cvar: &Condvar, guard: &mut MutexGuard<T>) {
         // The signature in `std` is quite a pain and CONSUMES the guard
         // We "move it out" for the duration of the wait as a hack
         unsafe {
@@ -22,7 +22,7 @@ pub(crate) mod imp {
         }
     }
 
-    pub fn mutex_lock<T>(mtx: &Mutex<T>) -> MutexGuard<T> {
+    pub(crate) fn mutex_lock<T>(mtx: &Mutex<T>) -> MutexGuard<T> {
         mtx.lock().expect("mutex poisoned (this is a bug)")
     }
 }
@@ -32,17 +32,17 @@ pub(crate) mod imp {
     pub use parking_lot::{Condvar, Mutex, MutexGuard};
 
     #[inline]
-    pub fn cvar_notify_one(cvar: &Condvar) {
+    pub(crate) fn cvar_notify_one(cvar: &Condvar) {
         let _ = cvar.notify_one();
     }
 
     #[inline]
-    pub fn cvar_wait<T>(cvar: &Condvar, guard: &mut MutexGuard<T>) {
+    pub(crate) fn cvar_wait<T>(cvar: &Condvar, guard: &mut MutexGuard<T>) {
         cvar.wait(guard);
     }
 
     #[inline]
-    pub fn mutex_lock<T>(mtx: &Mutex<T>) -> MutexGuard<T> {
+    pub(crate) fn mutex_lock<T>(mtx: &Mutex<T>) -> MutexGuard<T> {
         mtx.lock()
     }
 }
