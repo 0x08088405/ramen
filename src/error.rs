fcd2922c3bbab759b948777d42873b5c13ab8663
Adam <classygopher@gmail.com>
2022-08-02 15:29:03 +0100

    error: better errors, particularly in linux backend

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,19 @@
+use std::borrow::Cow;
+
 #[derive(Debug)]
 pub enum Error {
+    /// The request can't be completed because of a user error such as an invalid parameter or program state
+    Invalid,
+
     /// The program has run out of stack or a heap allocation has failed
     OutOfMemory,
 
-    /// TODO: ???
+    /// There system did not have the resources to fulfil the request
     SystemResources,
 
     /// A human-readable error string was returned by the operating system
-    Other(&'static str),
+    Text(Cow<'static, str>),
+
+    /// The platform does not support the features necessary for this request
+    Unsupported,
 }
--- src/platform/linux/imp.rs
+++ src/platform/linux/imp.rs
@@ -9,7 +9,10 @@ pub(crate) struct Window {
 
 impl Window {
     pub(crate) fn new(builder: &window::Builder) -> Result<Self, Error> {
+        // Check if XCB setup failed, possibly due to libxcb or an extension not being installed.
+        // This is recoverable - for example, the user might want to try Wayland setup if this fails.
         if XCB.is_valid() {
+            // Generate an ID and spawn a window with that ID
             let id = XCB.generate_id();
             let value_mask = ffi::XCB_CW_BACK_PIXEL | ffi::XCB_CW_EVENT_MASK;
             let value_list = &[
@@ -17,12 +20,15 @@ impl Window {
                 ffi::XCB_EVENT_MASK_KEY_PRESS | ffi::XCB_EVENT_MASK_KEY_RELEASE | ffi::XCB_EVENT_MASK_BUTTON_PRESS | ffi::XCB_EVENT_MASK_BUTTON_RELEASE,
             ];
             if XCB.create_window(id, 1, 1, 800, 608, 1, value_mask, value_list).is_err() {
-                return Err(Error::SystemResources) // TODO: ?
+                // xcb_create_window failed for some reason, presumably a bad user param
+                return Err(Error::Invalid)
             }
 
+            // Add WM_DELETE_WINDOW to WM_PROTOCOLS - important so we can hook the user clicking the X button
             let delete_prop = XCB.intern_atom(true, "WM_PROTOCOLS");
-            if delete_prop == ffi::XCB_ATOM_NONE { 
-                return Err(Error::SystemResources) // TODO: 
+            if delete_prop == ffi::XCB_ATOM_NONE {
+                // No WM_PROTOCOLS prop on window?
+                return Err(Error::Unsupported)
             }
             let delete_data = XCB.intern_atom(false, "WM_DELETE_WINDOW");
             XCB.change_property(
@@ -35,18 +41,24 @@ impl Window {
                 (&delete_data) as *const u32 as _,
             );
 
+            // Try to write the requested window title to the WM_NAME and _NET_WM_NAME properties
+            // Note: _NET_WM_NAME must be of type UTF8_STRING. We can only write it if UTF8_STRING prop type exists on
+            // the user's system. If it doesn't exist, and the title contains multibyte UTF8 characters, AND the user's
+            // system locale is not UTF8, then the title won't render properly. I don't care and neither should you.
             let title = builder.title.as_ref();
             let title_prop = XCB.intern_atom(false, "_NET_WM_NAME");
-            let title_prop_type = XCB.intern_atom(false, "UTF8_STRING");
-            XCB.change_property(
-                ffi::XCB_PROP_MODE_REPLACE,
-                id,
-                title_prop,
-                title_prop_type,
-                8,
-                title.bytes().len() as _,
-                title.as_ptr().cast(),
-            );
+            let title_prop_type = XCB.intern_atom(true, "UTF8_STRING");
+            if title_prop_type != ffi::XCB_ATOM_NONE {
+                XCB.change_property(
+                    ffi::XCB_PROP_MODE_REPLACE,
+                    id,
+                    title_prop,
+                    title_prop_type,
+                    8,
+                    title.bytes().len() as _,
+                    title.as_ptr().cast(),
+                );
+            }
             XCB.change_property(
                 ffi::XCB_PROP_MODE_REPLACE,
                 id,
@@ -57,6 +69,7 @@ impl Window {
                 title.as_ptr().cast(),
             );
 
+            // Get PID of current process and write that to _NET_WM_PID
             let pid = unsafe { libc::getpid() };
             let pid_prop = XCB.intern_atom(false, "_NET_WM_PID");
             XCB.change_property(
@@ -69,17 +82,21 @@ impl Window {
                 (&pid) as *const i32 as _,
             );
             
+            // Flush FFI requests. I'm not really sure what condition would cause an error here, if one even exists.
             if XCB.flush().is_err() {
-                return Err(Error::SystemResources) // TODO: ???
+                return Err(Error::Invalid)
             }
+
+            // And finally, try to map the window to the screen
+            // If successful the window will become visible at this point.
             if XCB.map_window(id).is_err() {
-                return Err(Error::SystemResources) // TODO: ??
+                return Err(Error::Invalid)
             }
             Ok(Window { _handle: id, event_queue: Vec::with_capacity(64) })
         } else {
             match ffi::dl_error() {
-                Some(s) => Err(Error::Other(s)),
-                None => Err(Error::SystemResources), // TODO: ?, ?
+                Some(s) => Err(Error::Text(s.into())),
+                None => Err(Error::Unsupported),
             }
         }
     }
