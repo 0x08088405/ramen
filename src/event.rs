4088e65153f4ae9e6be876f4174b0a8bc3450bb8
Adam <classygopher@gmail.com>
2022-09-16 08:32:47 +0100

    event: doc comments

--- src/event.rs
+++ src/event.rs
@@ -11,9 +11,13 @@ pub enum Event {
     /// The window focus state has been updated (`true` if focused).
     Focus(bool),
 
+    /// The window was resized.
     Resize((u16, u16)),
 
     /// A [`Key`] was pressed.
+    /// 
+    /// This event will tell you which physical key the user has pressed. If your aim is to process text input,
+    /// consider using the [`Event::Input`] event type instead.
     #[cfg_attr(feature = "nightly-rustdoc", doc(cfg(feature = "input")))]
     #[cfg_attr(not(feature = "nightly-rustdoc"), cfg(feature = "input"))]
     KeyboardDown(Key),
@@ -28,18 +32,22 @@ pub enum Event {
     #[cfg_attr(not(feature = "nightly-rustdoc"), cfg(feature = "input"))]
     KeyboardUp(Key),
 
+    /// A [`char`] was typed using the keyboard.
     #[cfg_attr(feature = "nightly-rustdoc", doc(cfg(feature = "input")))]
     #[cfg_attr(not(feature = "nightly-rustdoc"), cfg(feature = "input"))]
     Input(char),
 
+    /// A [`MouseButton`] was pressed.
     #[cfg_attr(feature = "nightly-rustdoc", doc(cfg(feature = "input")))]
     #[cfg_attr(not(feature = "nightly-rustdoc"), cfg(feature = "input"))]
     MouseDown(MouseButton),
 
+    /// A [`MouseButton`] was released.
     #[cfg_attr(feature = "nightly-rustdoc", doc(cfg(feature = "input")))]
     #[cfg_attr(not(feature = "nightly-rustdoc"), cfg(feature = "input"))]
     MouseUp(MouseButton),
 
+    /// The mouse was moved.
     #[cfg_attr(feature = "nightly-rustdoc", doc(cfg(feature = "input")))]
     #[cfg_attr(not(feature = "nightly-rustdoc"), cfg(feature = "input"))]
     MouseMove((u16, u16)),
