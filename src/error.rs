c14c2182892886bf223e256e1847daa753c121f9
viri <hi@viri.moe>
2022-09-19 19:09:57 -0600

    make error actually an error lol

--- src/error.rs
+++ src/error.rs
@@ -20,3 +20,10 @@ pub enum Error {
     /// The platform does not support the features necessary for this request
     Unsupported,
 }
+
+impl std::error::Error for Error {}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
